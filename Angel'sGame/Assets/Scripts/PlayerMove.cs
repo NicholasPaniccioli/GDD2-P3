13468e190c9c5983b6a2dd4392fe9559f933badd
Steven <sgm4689@IGM2025-12>
2019-11-12 13:59:16 -0500

    added roation and movement

---
+++ Angel'sGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Vector3 velocity;
+    [SerializeField]
+    private float dragForce = 0.8f, maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move up or down
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += Vector3.up;
+            transform.rotation = Quaternion.Euler(45, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            velocity += Vector3.down;
+            transform.rotation = Quaternion.Euler(-45, 0, 0);
+        }
+        //move right or left
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += Vector3.left;
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            velocity += Vector3.right;
+            transform.rotation = Quaternion.Euler(0, -135, 0);
+        }
+        //deceleration
+        velocity *= dragForce;
+        if (velocity.magnitude <= 0.1)
+            //if the velocity vector is to small to be noticable, stop moving
+            velocity = Vector3.zero;
+
+        //max velocity
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
+    }
+}
---
+++ Angel'sGame/Assets/Scripts/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private bool debug;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angleOfRotation = Mathf.Rad2Deg * Mathf.Atan2(mouseWorldPos.y - transform.position.y, mouseWorldPos.x - transform.position.x) - 90;
+        transform.rotation = Quaternion.Euler(0, 0, angleOfRotation);
+
+        if (Input.GetKeyDown(KeyCode.P))
+            debug = !debug;
+        if (debug)
+            drawDebug();
+    }
+
+    void drawDebug()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.up, Color.green);
+        Debug.DrawLine(transform.position, transform.position + transform.right, Color.red);
+
+    }
+}
