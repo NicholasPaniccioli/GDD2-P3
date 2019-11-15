34fa8bb5f2701def9d32f3ff0b5ae79956506e24
Nicholas Paniccioli <njp7745@g.rit.edu>
2019-11-15 09:49:35 -0500

    Added a collider to the player model and started a playtest scene. Also added a script for camera to follow player movement.

---
+++ Angel'sGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera cam; //Cam that follows the player around
+    public GameObject obj; //Object that the camera follows 
+    private Vector3 temp; //Temp vector to help with transform
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = cam.transform.position;
+
+        //Sets starting position of camera on player
+        temp.x = obj.transform.position.x;
+        temp.y = obj.transform.position.y;
+        temp.z = -10;
+        cam.transform.position = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Updates the position consistently
+        temp.x = obj.transform.position.x;
+        temp.y = obj.transform.position.y;
+        cam.transform.position = temp;
+    }
+}
