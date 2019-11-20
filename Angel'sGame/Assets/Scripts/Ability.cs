36041b4839f3d4d12ec3561116f5cc0bc440ed21
Steven <sgm4689@IGM2025-31>
2019-11-19 23:43:13 -0500

    continued to hate Ability script

--- Angel'sGame/Assets/Scripts/Ability.cs
+++ Angel'sGame/Assets/Scripts/Ability.cs
@@ -6,6 +6,7 @@ public abstract class Ability : MonoBehaviour
 {
     //Eventually this should have methods for determining if the ability is on cooldown, increasing and decreasing control of the character, and destroying the object whenever the spell runs out
     //however that's going to involve rewriting stuff that I'm not doing tonight, so rn this is literally an entire script to hold a float.
+    //Tl:DR  Don't kill me Jacob.  ~Steven
     [SerializeField]
     protected float damage;
     [SerializeField]
