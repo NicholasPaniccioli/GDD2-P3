4e63346246d6c48ddd92c2aec4455f77b326d268
Steven <sgm4689@g.rit.edu>
2019-11-25 09:49:42 -0500

    started ghoul

---
+++ Angel'sGame/Assets/Scripts/GhoulManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhoulManager : MonoBehaviour
+{
+    private GameObject[] pathList;
+    private GameObject currentNode;
+    private CombatManager combatManager;
+    [SerializeField]
+    private GameObject dresden, ghoulPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathList = GameObject.FindGameObjectsWithTag("Path");
+        combatManager = gameObject.GetComponent<CombatManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (dresden.transform.position.y > -36.95f)
+
+    }
+}
